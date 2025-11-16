f114c6da5f85473b002af4cba0931610aad6731d
OllieWP634 <172636129+OllieWP634@users.noreply.github.com>
2025-11-16 19:42:24 +0000

    Add boiler plate for fastAPI implementation

---
+++ app/main.py
@@ -0,0 +1,23 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+# NextJS server origin
+# what domains can send requests to this API
+origins = [
+    "http://localhost:3000"
+]
+
+# CORS middleware
+# Security, only allows the fronend to call this API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
