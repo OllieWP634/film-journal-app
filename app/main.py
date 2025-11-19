2eb9283928c2986d38d1f5e1f63141f6d1aae670
OllieWP634 <172636129+OllieWP634@users.noreply.github.com>
2025-11-19 00:12:05 +0000

    Create virtual environment for FastAPI

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
+import sqlite3
 
 app = FastAPI()
 
@@ -21,16 +22,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
-
-class FilmInput(BaseModel):
-    films: list[str]
-
-@app.post("/submit-films")
-def submit_films(input: FilmInput):
-    recommended = generate_recommendations(input.films)
-    return {"recommended": recommended}
-
-# input.films = ["Inception, Shawshank, Star Wars"]
-
-def generate_recommendations():
-    return
