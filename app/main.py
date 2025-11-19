dc189d2818e53bd1d81e8bac88d8e7cbea75f39c
OllieWP634 <172636129+OllieWP634@users.noreply.github.com>
2025-11-19 00:21:07 +0000

    Create BaseModel classes for TMDB results and SearchResponse

--- app/main.py
+++ app/main.py
@@ -1,24 +1,32 @@
-import uvicorn
-from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
+import httpx
 from pydantic import BaseModel
 from typing import List
-import sqlite3
+
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+TMDB_IMAGE_BASE = os.getenv("TMDB_IMAGE_BASE", "https://image.tmdb.org/t/p/w500")
 
 app = FastAPI()
-
-# NextJS server origin
-# what domains can send requests to this API
-origins = [
-    "http://localhost:3000"
-]
 
 # CORS middleware
 # Security, only allows the fronend to call this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_origins=["*"],
+    allow_methods=["GET"],
     allow_headers=["*"]
 )
+
+class TMDBResult(BaseModel):
+    id: int
+    title: str
+    year: str | None
+    overview: str | None
+    poster_url: str | None
+
+class SearchResponse(BaseModel):
+    query: str
+    results: List[TMDBResult]
